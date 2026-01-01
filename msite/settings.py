85d951bb3f445b4e9e9ac1a737032b9bc8fa21e1
mamdouh <Mamdooh@users.noreply.github.com>
2026-01-01 16:52:13 +0300

    fixed allowed hosts from localhost to all

--- msite/settings.py
+++ msite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-!gyfb^ldqf8f@prp1f%2b1&nqxn(6=*=^!pqphjgozzu^57yxr'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production !
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
